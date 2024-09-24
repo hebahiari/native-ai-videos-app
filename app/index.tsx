4eb8af0ddca1fbec399ba200c2c31d51a53a164c
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-09-24 13:30:08 -0700

    updated theme

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1'>
-      <Text className='text-3xl'>
+    <View className="flex-1 items-center justify-center bg-yellow-400">
+      <Text className='text-3xl font-pblack'>
         Heba's New App!
       </Text>
       <Link href='/profile' style={{color: 'blue'}}>Profile</Link>
